e9de2789e20fdea963efb0b0d7de38ccc6a8f74b
Jeremy Buck <jerbuck@cisco.com>
2021-09-28 09:46:10 -0600

    removed separate unit tests folder

--- dnac_sdk_playground/credentials_test.py
+++ dnac_sdk_playground/credentials_test.py
@@ -3,10 +3,7 @@ import unittest
 import sys
 import os
 
-# Update system path to dnac_sdk_playground package
-sys.path.append(os.pardir)
-sys.path.append(os.pardir + "/dnac_sdk_playground")
-from dnac_sdk_playground.credentials import Credentials
+from credentials import Credentials
 
 class Test_Credentials(unittest.TestCase):
     def test_invalid_file_name_verify_sys_exit_called(self):
@@ -18,7 +15,7 @@ class Test_Credentials(unittest.TestCase):
             credentials = Credentials(".creds_malformed")
 
     def test_valid_file_name_verify_base_url_populated(self):
-        credentials = Credentials("../.creds")
+        credentials = Credentials(".creds")
         self.assertEqual(credentials.baseUrl, "https://sandboxdnac.cisco.com/dna/intent/api/v1/")
 
 if __name__ == '__main__':
