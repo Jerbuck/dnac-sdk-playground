2ad1112ebaf9f0ff8be7a68b09c416b4130c1743
Jeremy Buck <jerbuck@cisco.com>
2021-09-27 23:23:39 -0600

    working unit test

---
+++ dnac_sdk_playground/credentials_test.py
@@ -0,0 +1,19 @@
+import unittest
+import os
+from credentials import Credentials
+
+class Test_Credentials(unittest.TestCase):
+    def test_invalid_file_name_verify_sys_exit_called(self):
+        with self.assertRaises(SystemExit):
+            credentials = Credentials("badPath")
+    
+    def test_malformed_json_file_verify_sys_exit_called(self):
+        with self.assertRaises(SystemExit):
+            credentials = Credentials(".creds_malformed")
+
+    def test_valid_file_name_verify_base_url_populated(self):
+        credentials = Credentials(".creds")
+        self.assertEqual(credentials.baseUrl, "https://sandboxdnac.cisco.com/dna/intent/api/v1/")
+
+if __name__ == '__main__':
+    unittest.main()
